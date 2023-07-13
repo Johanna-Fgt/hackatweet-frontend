21ab1306f029f4b440a45e39e4aefd924104b93c
johanna <johannafaget@gmail.com>
2023-07-13 12:07:07 +0200

    signin done

--- components/Signin.js
+++ components/Signin.js
@@ -11,25 +11,76 @@ const SignIn = () => {
 	const [isModalVisible, setIsModalVisible] = useState(false);
 
 	const handleConnection = () => {
-		fetch('http://localhost:3000/users/signin', {
+		const URL = 'http://localhost:3000/users/signin';
+		const config = {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
 				username: signInUsername,
 				password: signInPassword,
 			}),
-		})
+		};
+
+		fetch(URL, config)
 			.then((response) => response.json())
 			.then((data) => {
 				if (data.result) {
-					dispatch(login({ username: signInUsername, token: data.token }));
+					dispatch(
+						login({
+							firstname: data.firstname,
+							username: signInUsername,
+							token: data.token,
+						})
+					);
 					setSignInUsername('');
 					setSignInPassword('');
 					setIsModalVisible(false);
 				}
 			});
 	};
-	return <></>;
+
+	let modalContent = (
+		<div className={styles.registerContainer}>
+			<button type="button" onClick={() => setIsModalVisible(!isModalVisible)}>
+				X
+			</button>
+			<div className={styles.registerSection}>
+				<p>Connect to Hackatweet</p>
+
+				<input
+					type="text"
+					placeholder="Username"
+					onChange={(e) => setSignInUsername(e.target.value)}
+					value={signInUsername}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					onChange={(e) => setSignInPassword(e.target.value)}
+					value={signInPassword}
+				/>
+				<button onClick={handleConnection}>Sign in</button>
+			</div>
+		</div>
+	);
+
+	return (
+		<div className={styles.signup}>
+			<button type="button" onClick={() => setIsModalVisible(!isModalVisible)}>
+				Sign in
+			</button>
+			{isModalVisible && (
+				<Modal
+					getContainer="#react-modals"
+					className={styles.modal}
+					open={isModalVisible}
+					closable={false}
+					footer={null}>
+					{modalContent}
+				</Modal>
+			)}
+		</div>
+	);
 };
 
 export default SignIn;
