1c9bd1fe7664664d82c9702d3ddad62d4951d7a7
nadia11232000 <135129710+nadia11232000@users.noreply.github.com>
2023-07-13 16:04:02 +0200

    Twwet commit

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,13 +1,15 @@
-// import styles from '../styles/Login.module.css';
+import styles from "../styles/Login.module.css";
+import { useSelector } from "react-redux";
 
 const Tweet = () => {
-	// const user = useSelector((state) => state.user.value);
+  const user = useSelector((state) => state.user.value);
 
-	return (
-		<div>
-			<h1>Tweet</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Tweet</h1>
+      {user.username}
+    </div>
+  );
 };
 
 export default Tweet;
