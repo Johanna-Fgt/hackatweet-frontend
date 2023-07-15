fdf51c11e15904be2fcc7ead7481d4f3a7832469
johanna <johannafaget@gmail.com>
2023-07-15 12:20:27 +0200

    seo extra light

--- pages/_app.js
+++ pages/_app.js
@@ -27,6 +27,40 @@ function App({ Component, pageProps }) {
 			<PersistGate persistor={persistor}>
 				<Head>
 					<title>Hackatweet</title>
+					<link rel="icon" href="/favicon.ico" />
+					<meta name="title" content="Hackatweet" />
+					<meta
+						name="description"
+						content="Second hackathon des éléves de la Capsule !"
+					/>
+					<meta
+						name="viewport"
+						content="initial-scale=1.0, width=device-width"
+					/>
+
+					<meta property="og:type" content="website" />
+					<meta property="og:url" content="https://metatags.io" />
+					<meta property="og:title" content="Hackatweet" />
+					<meta
+						property="og:description"
+						content="Second hackathon des éléves de la Capsule !"
+					/>
+					<meta
+						property="og:image"
+						content="https://metatags.io/images/meta-tags.png"
+					/>
+
+					<meta property="twitter:card" content="summary_large_image" />
+					<meta property="twitter:url" content="https://metatags.io" />
+					<meta property="twitter:title" content="Hackatweet" />
+					<meta
+						property="twitter:description"
+						content="Second hackathon des éléves de la Capsule !"
+					/>
+					<meta
+						property="twitter:image"
+						content="https://metatags.io/images/meta-tags.png"
+					/>
 				</Head>
 				<Component {...pageProps} />
 			</PersistGate>
